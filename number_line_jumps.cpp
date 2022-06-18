89b9b34fc8dd34625861f88c854b1593d43ff6f9
calinanca99 <calinanca99@gmail.com>
2022-06-18 17:27:48 +0200

    Solve "Number Line Jumps"

---
+++ number_line_jumps.cpp
@@ -0,0 +1,13 @@
+string kangaroo(int x1, int v1, int x2, int v2) {
+    if ((x1 < x2 && v1 < v2) || (x2 < x1 && v2 < v1)) {
+        return std::string("NO");
+    }
+    
+    if (v1 == v2 && x1 != x2) {
+        return std::string("NO");
+    }
+    
+    // This result is always positive since x2 > x1 and it has been already
+    // checked that v1 > v2.
+    return (x2 - x1) % (v1 - v2) == 0 ? std::string("YES") : std::string("NO");
+}
